edfa493423fa652c5cf285f72fb39220397d6c2b
anqli <anqli@outlook.com>
2018-10-23 11:45:26 +0800

    first commit

--- spring-cloud-project/config-client/src/main/java/com/msa/configclient/ConfigClientApplication.java
+++ spring-cloud-project/config-client/src/main/java/com/msa/configclient/ConfigClientApplication.java
@@ -3,14 +3,13 @@ package com.msa.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 
 /**
  * @author anquan li
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@RefreshScope
 public class ConfigClientApplication {
 
   public static void main(String[] args) {
